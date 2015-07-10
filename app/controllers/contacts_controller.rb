c6ba5a2ca9bcf9e1c5cc71f1779757e600ec2b95
disacoty <disa.coty@gmail.com>
2015-07-10 06:37:56 +0000

    Added contact form validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,20 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
   def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save 
+      flash[:success] = 'Message sent'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Error, missing valid name or email'
+      redirect_to new_contact_path
   end
+  end
+  
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :email, :comments)
+  end
+  
 end
